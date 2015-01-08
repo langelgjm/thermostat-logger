126ef86548390717328f14d1627a8ac9313f1f5c
Gabriel J. Michael <gabriel.j.michael@gmail.com>
2015-01-08 13:56:52 -0500

    Added callback code

--- thermostat_monitor.py
+++ thermostat_monitor.py
@@ -1,21 +1,35 @@
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 import datetime
-GPIO.setmode(GPIO.BOARD)  
-  
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-print "Monitoring the thermostat through the HCPL3700...\n"
-print str(datetime.datetime.now())
-while True:
-	try:  
-		GPIO.wait_for_edge(11, GPIO.FALLING)  
-		print str(datetime.datetime.now())
-		print "\nFalling edge detected."  
-		print " Looks like the heating turned on!"
-		GPIO.wait_for_edge(11, GPIO.RISING)
-		print str(datetime.datetime.now())
-		print "\nRising edge detected."  
-		print " Looks like the heating turned off!"
-	except KeyboardInterrupt:  
-		GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
-		print "\n"
-GPIO.cleanup()           # clean up GPIO on normal exit  
+import time
+
+def cb_therm_status(channel):
+        print str(datetime.datetime.now())
+        if GPIO.input(channel):
+                print "Rising edge detected. Looks like the heating turned OFF."
+        else:
+                print "Falling edge detected. Looks like the heating turned ON."
+
+try:
+	HCPL3700 = 11
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(HCPL3700, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	
+	print str(datetime.datetime.now())
+	print "Monitoring the thermostat with the HCPL3700..."
+	
+	# Initially just poll once to get the status of the line
+	print "Getting initial status:"
+	cb_therm_status(HCPL3700)
+	
+	GPIO.add_event_detect(HCPL3700, GPIO.BOTH, callback=cb_therm_status, bouncetime=100)
+	
+	while True:
+		# Event detection should take place in this never-ending loop
+		print "Sleeping..."
+		time.sleep(60)
+except KeyboardInterrupt:
+	pass
+	# clean up GPIO on CTRL+C exit
+finally:
+	print "Cleaning up GPIO..."
+	GPIO.cleanup()
